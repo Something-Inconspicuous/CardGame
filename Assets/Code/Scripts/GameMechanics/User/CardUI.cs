789813dc11ca8e0a0735a4eec0c23f1be805dcf8
Something-Inconspicuous <webhead220@gmail.com>
2023-05-21 14:34:50 -0400

    Add some card ui stuff as well as folders

---
+++ Assets/Code/Scripts/GameMechanics/User/CardUI.cs
@@ -0,0 +1,54 @@
+using UnityEngine;
+
+namespace GameMechanics.User {
+    public class CardUI : MonoBehaviour {
+
+        private const float changeXDef = 0f;
+        private const float changeYDef = 0f;
+        private const float changeZDef = 0f;
+        
+        [Header("Change on Hover")]
+        [SerializeField] private float changeX = changeXDef;
+        [SerializeField] private float changeY = changeYDef;
+        [SerializeField] private float changeZ = changeZDef;
+
+        private Vector3 upPos;
+        private Vector3 downPos;
+        private bool isUp;
+
+        private const float smoothing = 0.1f;
+
+        void Awake() {
+            upPos = transform.position + new Vector3(changeX, changeY, changeZ);
+            downPos = transform.position;
+        }
+
+        void OnMouseEnter() {
+            isUp = true;
+        }
+
+        void OnMouseExit() {
+            isUp = false;
+        }
+
+        private Vector3 velocity = new Vector3();
+        void Update() {
+            Debug.Log(isUp);
+            if(isUp){
+                transform.position = Vector3.SmoothDamp(
+                    current: transform.position,
+                    target: upPos,
+                    currentVelocity: ref velocity,
+                    smoothTime: smoothing
+                );
+            } else {
+                transform.position = Vector3.SmoothDamp(
+                    current: transform.position,
+                    target: downPos,
+                    currentVelocity: ref velocity,
+                    smoothTime: smoothing
+                );
+            }
+        }
+    }
+}
