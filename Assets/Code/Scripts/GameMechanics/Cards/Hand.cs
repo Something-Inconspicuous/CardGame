5710996cfb2b8c445394703148884d3a15151b84
imacrazyguy412 <110923269+imacrazyguy412@users.noreply.github.com>
2023-05-22 13:36:06 -0400

    we dont talk about it

--- Assets/Code/Scripts/GameMechanics/Cards/Hand.cs
+++ Assets/Code/Scripts/GameMechanics/Cards/Hand.cs
@@ -1,6 +1,12 @@
 
 namespace GameMechanics.Cards {
     public class Hand {
+        //probably will not be a constant if we have variable hand size as a power eg. drawing more cards per turn
         public const int MAX_CARDS = 7;
+        
+        public Hand(){
+            
+        }
+        
     }    
 }
